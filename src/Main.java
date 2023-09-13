ea89a2902f71520cfc219e6b38b7558d99965536
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-13 09:37:48 -0400

    Initial

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int x=1; x <= 12; x++) {
+            System.out.println(x + " " + x*2 + " " + x*3 + " " + x*4 + " " + x*5 + " " + x*6 + " " + x*7 + " " + x*8 + " " + x*9 + " " + x*10 + " " + x*11 + " " + x*12);
+        }
+    }
+}
